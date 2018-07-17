ab31aa31654a4d435f80e115e4c7faad5da37918
Brett Vonbbr <vonbbr@yahoo.com>
2018-07-17 14:17:06 -0500

    Add image change script and image

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,11 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/Firefox-Logo.png') {
+        myImage.setAttribute ('src', 'images/firefox-logo-2.png');
+    } else {
+        myImage.setAttribute ('src', 'images/Firefox-Logo.png');
+    }
+}
